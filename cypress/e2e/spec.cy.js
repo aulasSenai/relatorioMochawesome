d637d98bf322bdc0eea5ad0f8ec6e81301d25afb
BenHurJF <beiujeffer@hotmail.com>
2023-05-24 12:57:09 -0300

    Atualizando arquivo azure-pipelines.yml

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -16,7 +16,8 @@ describe('template spec', () => {
   })
 
   it('Teste 5', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://example.cypress.io').url()
+    .then(stringUpperCase = ()  => console.log(stringUpperCase.toUpperCase))
   })
 
   it('Teste 7', () => {
